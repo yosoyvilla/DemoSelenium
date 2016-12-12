ff0bca109020e3cd0a524fd772307617bf3bf52a
dvillar <dvillar@psl.com.co>
2016-12-12 14:51:26 -0500

    Se agrega un ejemplo de espera

--- src/test/java/org/seleniumdemo/po/LoginPO.java
+++ src/test/java/org/seleniumdemo/po/LoginPO.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
@@ -22,5 +23,9 @@ public class LoginPO {
 	
 	@FindBy(id = "")
 	private WebElement campoUsuario;
+	
+	private void esperarElemento(WebElement elemento) {
+		fluentWait.until(ExpectedConditions.visibilityOf(elemento));
+	}
 
 }
